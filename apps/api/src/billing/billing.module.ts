75d614f35b1ae92f22f80db24f6c377509ba1db5
mixawang-stack~ <mixa.wang@gmail.com~>
2026-01-27 20:38:48 +0800

    fix: wire auth module into billing

--- apps/api/src/billing/billing.module.ts
+++ apps/api/src/billing/billing.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../prisma.module";
+import { AuthModule } from "../auth/auth.module";
 import { CreemController } from "./creem.controller";
 import { CreemService } from "./creem.service";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [CreemController],
   providers: [CreemService]
 })
