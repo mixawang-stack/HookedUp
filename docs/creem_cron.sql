1eca51b2b235a603937c7d32ecf860b2fb6c8752
mixawang-stack~ <mixa.wang@gmail.com~>
2026-02-04 15:47:46 +0800

    Add Supabase pg_cron SQL for Creem processing

---
+++ docs/creem_cron.sql
@@ -0,0 +1,38 @@
+-- Enable required extensions
+create extension if not exists pg_cron;
+create extension if not exists http;
+
+-- Function to invoke Creem processor endpoint
+create or replace function public.invoke_process_creem_events()
+returns void
+language plpgsql
+as $$
+declare
+  resp json;
+begin
+  select content::json
+  into resp
+  from http(
+    (
+      'POST',
+      'https://YOUR_PRODUCTION_DOMAIN/api/cron/process-creem-events',
+      ARRAY[
+        http_header('x-cron-secret', 'YOUR_CRON_SECRET')
+      ],
+      'application/json',
+      '{}'::text
+    )::http_request
+  );
+end;
+$$;
+
+-- Schedule every minute
+select cron.schedule(
+  'process_creem_events_every_minute',
+  '* * * * *',
+  $$select public.invoke_process_creem_events();$$
+);
+
+-- Verify jobs
+-- select * from cron.job;
+-- select * from cron.job_run_details order by start_time desc limit 20;
